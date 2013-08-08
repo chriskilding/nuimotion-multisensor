bb6c3d48c2bb9ac4e04e9513a7fe1e5a5f3089db
Christopher Kilding <mr.chris.k@gmail.com>
2013-08-08 17:25:22 +0100

    pose detected test

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ var context = OpenNI();
 
 // var converter = require("./nuimotion-zig-adapter");
 
-context.emit = function () {
-  console.log('emitted', arguments);
-};
+/*context.emit = function () {
+    console.log('emitted', arguments);
+};*/
+
+context.on("posedetected", function () {
+    console.log('User emitted event: ', arguments);
+});
 
 process.on('SIGINT', function () {
-  context.close();
-  process.exit();
+    context.close();
+    process.exit();
 });
