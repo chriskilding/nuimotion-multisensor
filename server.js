053c021eed8ab25b67b531dc2a4a8e929cdc244c
Christopher Kilding <mr.chris.k@gmail.com>
2013-08-08 09:31:38 +0100

    twas bad json

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+/*jslint node: true */
+
+"use strict";
+
 console.log("starting");
 var nuimotion = require("nuimotion");
+var zig = require("./zigJoints");
 // var converter = require("./nuimotion-zig-adapter")
 console.log("joints", nuimotion);
-console.log("zig", require("./zigJoints"));
+console.log("zig", zig.Head);
 
 var onSkeletonUpdate = function (skel) {
-    converter.convert(skel);
-}
+    // converter.convert(skel);
+};
 
 // listen to the whole array
 nuimotion.startSkeletonListener(nuimotion.Joints, onSkeletonUpdate, 10000);
