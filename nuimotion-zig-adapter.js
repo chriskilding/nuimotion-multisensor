02fae16f2f3885afc95c283080a5de4c50d63b93
Christopher Kilding <mr.chris.k@gmail.com>
2013-08-08 09:43:08 +0100

    nope, can't have { x.y: z }

--- nuimotion-zig-adapter.js
+++ nuimotion-zig-adapter.js
@@ -3,24 +3,10 @@
 
 var _ = require("underscore");
 
-// The ZigJS joints
-var zigJoints = require("./zigJoints");
-
 var nuimotion = require("nuimotion");
 
 // Mapping of nuijoints to ZigJS joints
-var jointMappings = {
-    nuimotion.Joints.HEAD: zigJoints.Head,
-    nuimotion.Joints.LEFT_HAND: zigJoints.LeftHand,
-    nuimotion.Joints.RIGHT_HAND: zigJoints.RightHand,
-    nuimotion.Joints.LEFT_ELBOW: zigJoints.LeftElbow,
-    nuimotion.Joints.RIGHT_ELBOW: zigJoints.RightElbow,
-    nuimotion.Joints.LEFT_SHOULDER: zigJoints.LeftShoulder,
-    nuimotion.Joints.RIGHT_SHOULDER: zigJoints.RightShoulder,
-    nuimotion.Joints.LEFT_HIP: zigJoints.LeftHip,
-    nuimotion.Joints.RIGHT_HIP: zigJoints.RightHip,
-    nuimotion.Joints.TORSO: zigJoints.Torso;
-};
+var jointMappings = require("./jointMappings");
 
 // Adapts the NUIMotion data format to that of ZigJS
 // which is what we happen to be using elsewhere on our own project.
