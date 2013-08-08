de5a2db59fa14ef56107b09beef7dc03b241236b
Christopher Kilding <mr.chris.k@gmail.com>
2013-08-08 09:35:15 +0100

    is zis legit?

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 /*jslint node: true */
-
 "use strict";
 
 console.log("starting");
 var nuimotion = require("nuimotion");
-var zig = require("./zigJoints");
-// var converter = require("./nuimotion-zig-adapter")
-console.log("joints", nuimotion);
-console.log("zig", zig.Head);
+var converter = require("./nuimotion-zig-adapter");
+console.log("nuimotion", nuimotion);
 
 var onSkeletonUpdate = function (skel) {
-    // converter.convert(skel);
+    converter.convert(skel);
 };
 
 // listen to the whole array
